a094ad7718f8cae9c21143d4c8910f8ad3c622c1
nespolisimmaco <snespoli@yahoo.com>
2023-03-28 18:07:53 +0200

    Added a message and an image to DOM with data

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: "Hello Vue Js!",
+            imgSrc: "https://picsum.photos/300",
+            imgDesc: "Random image"
+        }
+    },
+    methods: {
+
+    }
+}).mount("#app");
